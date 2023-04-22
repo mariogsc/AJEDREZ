2173285b1eb73b8ef1fed0f726344c72c1a9c09c
MARIO GÓMEZ SÁNCHEZ-CELEMÍN <mario.gomez.sanchez-celemin@alumnos.upm.es>
2023-04-22 18:05:13 +0200

    Update Tablero.h

--- scr/Tablero.h
+++ scr/Tablero.h
@@ -1,14 +1,15 @@
 
-class Tablero {
-private:	
-	unsigned char rojo;
-	unsigned char verde;
-	unsigned char azul;
-
-	float x;
-	float y;
-	float z;
-public:
-	void DibujarCuadrado(float x, float y, float z, float tam);
-	void DibujarTablero();
+class Tablero
+{
+	float x=-20.0;
+	float y=-20.0;
+	float z=0.0;
+	float tam = 5.0;
+	int rojo;
+	int azul;
+	int verde;
+public: 
+	void DibujaTablero(float tam);
+	void DibujaCuadrado(float x, float y, float z,float tam);
+	void SetColor(int, int);
 };
