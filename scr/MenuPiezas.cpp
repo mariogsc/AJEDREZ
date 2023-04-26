e2e338786d674cdc503df23768c6ba555ed2f761
Álvaro Gómez Agudo <80710074+alvarogomez29@users.noreply.github.com>
2023-04-26 12:04:36 +0200

    Update MenuPiezas.cpp

--- scr/MenuPiezas.cpp
+++ scr/MenuPiezas.cpp
@@ -2,45 +2,62 @@
 #include <ETSIDI.h>
 #include "freeglut.h"
 
-Menu::Menu(){
-glColor3ub(0, 0, 0);
-glDisable(GL_LIGHTING);
-glBegin(GL_POLYGON);
-glVertex3f(41.3f, 37.3, 0.0f);
-glVertex3f(41.3f, 39.7, 0.0f);
-glVertex3f(47.85f, 39.7, 0.0f);
-glVertex3f(47.85f, 37.3, 0.0f);
-glEnd();
-glEnable(GL_LIGHTING);
 
-glColor3ub(0, 0, 0);
-glDisable(GL_LIGHTING);
-glBegin(GL_POLYGON);
-glVertex3f(48.15f, 37.3, 0.0f);
-glVertex3f(48.15f, 39.7, 0.0f);
-glVertex3f(54.7f, 39.7, 0.0f);
-glVertex3f(54.7f, 37.3, 0.0f);
-glEnd();
-glEnable(GL_LIGHTING);
+void Menu::CreaMenu() {
 
-glColor3ub(0, 0, 0);
-glDisable(GL_LIGHTING);
-glBegin(GL_POLYGON);
-glVertex3f(41.3f, 0.3, 0.0f);
-glVertex3f(54.7f, 0.3, 0.0f);
-glVertex3f(54.7f, 37.0, 0.0f);
-glVertex3f(41.3f, 37.0, 0.0f);
-glEnd();
-glEnable(GL_LIGHTING);
+	CartelBlancas->draw();
+	CartelNegras->draw();
+
+	glColor3ub(0, 0, 0);
+	glDisable(GL_LIGHTING);
+	glBegin(GL_POLYGON);
+	glVertex3f(9.8f, 7.3, 0.0f);
+	glVertex3f(11.1f, 7.3, 0.0f);
+	glVertex3f(11.1f, 7.9, 0.0f);
+	glVertex3f(9.8f, 7.9, 0.0f);
+	glEnd();
+	glEnable(GL_LIGHTING);
+	
+	glColor3ub(0, 0, 0);
+	glDisable(GL_LIGHTING);
+	glBegin(GL_POLYGON);
+	glVertex3f(8.3f, 7.3, 0.0f);
+	glVertex3f(9.7f, 7.3, 0.0f);
+	glVertex3f(9.7f, 7.9, 0.0f);
+	glVertex3f(8.3f, 7.9, 0.0f);
+	glEnd();
+	glEnable(GL_LIGHTING);
+
+	glColor3ub(0, 0, 0);
+	glDisable(GL_LIGHTING);
+	glBegin(GL_POLYGON);
+	glVertex3f(9.8f, 0.1, 0.0f);
+	glVertex3f(11.1f, 0.1, 0.0f);
+	glVertex3f(11.1f, 7.2, 0.0f);
+	glVertex3f(9.8f, 7.2, 0.0f);
+	glEnd();
+	glEnable(GL_LIGHTING);
+
+	glColor3ub(0, 0, 0);
+	glDisable(GL_LIGHTING);
+	glBegin(GL_POLYGON);
+	glVertex3f(8.3f, 0.1, 0.0f);
+	glVertex3f(9.7f, 0.1, 0.0f);
+	glVertex3f(9.7f, 7.2, 0.0f);
+	glVertex3f(8.3f, 7.2, 0.0f);
+	glEnd();
+	glEnable(GL_LIGHTING);
+	
 
 
-glColor3ub(255, 255, 255);
-glDisable(GL_LIGHTING);
-glBegin(GL_POLYGON);
-glVertex3f(41.0f, 0.0, 0.0f);
-glVertex3f(55.0f, 0.0, 0.0f);
-glVertex3f(55.0f, 40.0, 0.0f);
-glVertex3f(41.0f, 40.0, 0.0f);
-glEnd();
-glEnable(GL_LIGHTING);
+	glColor3ub(255, 255, 255);
+	glDisable(GL_LIGHTING);
+	glBegin(GL_POLYGON);
+	glVertex3f(8.2f, 0.0, 0.0f);
+	glVertex3f(11.2f, 0.0, 0.0f);
+	glVertex3f(11.2f, 8, 0.0f);
+	glVertex3f(8.2f, 8, 0.0f);
+	glEnd();
+	glEnable(GL_LIGHTING);
+
 }
