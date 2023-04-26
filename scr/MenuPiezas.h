70024b0d5aa2432b87d8e63abaf5457b52a6f66f
Álvaro Gómez Agudo <80710074+alvarogomez29@users.noreply.github.com>
2023-04-26 12:03:44 +0200

    Update MenuPiezas.h

--- scr/MenuPiezas.h
+++ scr/MenuPiezas.h
@@ -1,8 +1,13 @@
 #pragma once
 #include <ETSIDI.h>
 #include "freeglut.h"
+using namespace ETSIDI;
+using ETSIDI::getTexture;
 
+using ETSIDI::Sprite;
 class Menu {
+	Sprite* CartelBlancas = new Sprite("imagenes/CartelBlancas.png", 9, 7.6, 1, 0.4);
+	Sprite* CartelNegras = new Sprite("imagenes/CartelNegras.png", 10.45, 7.6, 1, 0.4);
 public:
-	Menu();
-}; 
+	void CreaMenu();
+};
