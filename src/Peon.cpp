db03a4ba30846cf2c4f919f6d8df622dd0b97e1f
Iñigo Castells <126980526+inigocastells@users.noreply.github.com>
2023-05-01 17:36:24 +0200

    Update Peon.cpp

--- src/Peon.cpp
+++ src/Peon.cpp
@@ -2,20 +2,27 @@
 #include <ETSIDI.h>
 
 
-	void Peon::Dibuja() {
-		if (color == BLANCO) {
+void Peon::dibuja()
+{
+	//if (x == 1) {
+		if (color == COLOR::BLANCO) {
 			glDisable(GL_LIGHTING);
+			glTranslatef(posicion.y, -posicion.x - 6, 0);
 			ImagenPeonB.setCenter(0, 0);
 			ImagenPeonB.setSize(1, 1);
 			ImagenPeonB.draw();
+			glTranslatef(-posicion.y, posicion.x + 6, 0);
 			glEnable(GL_LIGHTING);
 		}
-		else if (color == NEGRO) {
+
+		if (color == COLOR::NEGRO) {
 			glDisable(GL_LIGHTING);
+			glTranslatef(posicion.y + 1, posicion.x - 2, 0);
 			ImagenPeonN.setCenter(0, 0);
 			ImagenPeonN.setSize(1, 1);
+			ImagenPeonN.flip(true, true);
 			ImagenPeonN.draw();
+			glTranslatef(-posicion.y - 1, -posicion.x + 2, 0);
 			glEnable(GL_LIGHTING);
 		}
-	}
-
+}
