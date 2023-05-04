c8bc6b9b7747c6514644f7ad9c5d9a7838d21a8d
MARIO GÓMEZ SÁNCHEZ-CELEMÍN <mario.gomez.sanchez-celemin@alumnos.upm.es>
2023-05-05 01:47:57 +0200

    Update Tablero.cpp
    Se añade impresión de piezas con vector dinámico capaz de agregarle piezas hasta maximo

--- src/Tablero.cpp
+++ src/Tablero.cpp
@@ -4,17 +4,21 @@
 #include <Casilla.h>
 
 
-Tablero::Tablero() {
+Tablero::Tablero():NPiezas(0) {
 	// Primero reservamos espacio dinamico para una matriz de 8x8 casillas (tablero) 
 	bool color = false;
 
 	casilla = new Casilla * [NCasillas];
 	
-	for (int i = 0; i < NCasillas; i++) {
+	for (int i = 0; i < NCasillas; i++) {  
 		casilla[i] = new Casilla[NCasillas];
 	}
+
+	// Inicializamos el vector de piezas 
+	inicializa();
 }
 
+// FUNCIÓN PARA DIBUJAR EL TABLERO UTILIZANDO UNA MATRIZ DE CASILLAS Y ELEGIR EL COLOR SEGÚN EL EQUIPO SELECCIONADO
 void Tablero::DibujaTablero(ColorCasilla color1, ColorCasilla color2) {
 	float tam= 1.0; //Definimos variables auxiliares para ir desplazando la casilla
 	Vector aux{0.0,0.0};
@@ -39,11 +43,38 @@ void Tablero::DibujaTablero(ColorCasilla color1, ColorCasilla color2) {
 			
 			casilla[columna][fila].DibujaCasilla(casilla[columna][fila].GetPos());
 
+			
 			aux.x += tam;
 		}
 	}
-	for (int j = 0; j < NCasillas; j++)
-	{
-		casilla[1][j].setPieza(new Peon(Vector(1, j), Piezas::NEGRO));
-	}
+
+}
+
+
+// FUNCIONES PARA EL MANEJO DEL VECTOR DE PIEZAS
+void Tablero::DibujaPiezas()
+{
+	//for (int i = 0; i < MAX_PIEZAS; i++)  // no se puede añadir hasta que se definan todas las piezas o hasta que se cree un tipo de piezas pieza vacía y se inicialice el vector
+		lista[0]->Dibuja();
+}
+
+void Tablero::mueve(Vector v) {
+	
+	//for (int i = 0; i < N_PIEZAS; i++)
+		// lista[i]->mueve(v); TODAVIA NO ESTA HECHO EL POLIMORFISMO DE MOVER
+	
+}
+
+bool Tablero::agregar(Piezas* p)
+{
+	if (NPiezas < MAX_PIEZAS)
+		lista[NPiezas++] = p; 
+	else
+		return false; // capacidad máxima alcanzada
+	return true;
+}
+
+void Tablero::inicializa() {  // inicializacion de todas las fichas e inclusión en el vector de piezas
+	Piezas* p1 = new Peon(Vector{ 0,0 }, Piezas::COLOR::BLANCO);
+	agregar(p1);
 }
