f85c7ad04316ead6ce3a3bf294bdbb21a3427b1f
MARIO GÓMEZ SÁNCHEZ-CELEMÍN <mario.gomez.sanchez-celemin@alumnos.upm.es>
2023-05-05 01:46:28 +0200

    Update Tablero.h

--- src/Tablero.h
+++ src/Tablero.h
@@ -4,16 +4,19 @@ using ETSIDI::Sprite;
 #include <Piezas.h>
 #include <ColorCasilla.h>
 
+#define MAX_PIEZAS 32
+
 class Tablero
 {
 	const int NCasillas = 8; // Creamos una constante del numero de casillas 
 	Casilla** casilla; // Puntero doble de tipo casilla para recorrer las casillas
-	Piezas* pieza; // Puntero de tipo Piezas para poder imprimirlas
-	
+	Piezas* lista[MAX_PIEZAS]; // Puntero de tipo Piezas para poder imprimirlas
+	int NPiezas;
 public: 
 	Tablero(); // Constructor por defecto de tablero
-
 	void DibujaTablero(ColorCasilla color1,ColorCasilla color2);
-	
-
+	void DibujaPiezas();
+	void mueve(Vector v); 
+	void inicializa();
+	bool agregar(Piezas* p);
 };
