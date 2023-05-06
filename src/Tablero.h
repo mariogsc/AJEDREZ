13cf092cfadc88615d8e5862162c60ce02fe864d
Iñigo Castells <126980526+inigocastells@users.noreply.github.com>
2023-05-06 11:00:44 +0200

    Update Tablero.h

--- src/Tablero.h
+++ src/Tablero.h
@@ -1,8 +1,11 @@
+#pragma once
 #include <ETSIDI.h>
 using ETSIDI::Sprite;
 #include <Casilla.h>
-#include <Piezas.h>
+#include <Piezas.h> 
 #include <ColorCasilla.h>
+#include <Peon.h>
+#include <Vector.h>
 
 #define MAX_PIEZAS 32
 
@@ -10,13 +13,16 @@ class Tablero
 {
 	const int NCasillas = 8; // Creamos una constante del numero de casillas 
 	Casilla** casilla; // Puntero doble de tipo casilla para recorrer las casillas
+	Casilla c;
 	Piezas* lista[MAX_PIEZAS]; // Puntero de tipo Piezas para poder imprimirlas
 	int NPiezas;
 public: 
 	Tablero(); // Constructor por defecto de tablero
 	void DibujaTablero(ColorCasilla color1,ColorCasilla color2);
 	void DibujaPiezas();
-	void mueve(Vector v); 
+	void mueve(); 
 	void inicializa();
 	bool agregar(Piezas* p);
+	void MueveCursor(unsigned char key);
+	void CheckPosition();
 };
