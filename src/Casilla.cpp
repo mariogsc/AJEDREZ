ab64606a82c0e30521293c0f595d28c7fc5d6703
Iñigo Castells <126980526+inigocastells@users.noreply.github.com>
2023-05-06 11:06:17 +0200

    Update Casilla.cpp

--- src/Casilla.cpp
+++ src/Casilla.cpp
@@ -6,16 +6,17 @@ Casilla::Casilla() :tam(1.0), pos(0.0, 0.0) {
 		p = nullptr; // Inicializamos todos los punteros de las fichas apuntando a nada (nullptr)
 	}
 }
-void Casilla::DibujaCasilla(Vector posicion) {
+void Casilla::DibujaCasilla(Vector posicion, float x) {
 
 	glBegin(GL_POLYGON);
-	glVertex3f(posicion.x, posicion.y, 0.0);
-	glVertex3f(posicion.x + tam, posicion.y, 0.0);
-	glVertex3f(posicion.x + tam, posicion.y + tam, 0.0);
-	glVertex3f(posicion.x, posicion.y + tam, 0.0);
+	glVertex3f(posicion.x, posicion.y, x);
+	glVertex3f(posicion.x + tam, posicion.y, x);
+	glVertex3f(posicion.x + tam, posicion.y + tam, x);
+	glVertex3f(posicion.x, posicion.y + tam, x);
 	glEnd();
 
 }
+
 
 void Casilla::SetColor(unsigned char r ,unsigned char v, unsigned char a) {
 	this->color.SetColor(r, v, a);
