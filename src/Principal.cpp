482738715cf0b9e237d13ff1459c8120ebe64b4f
Iñigo Castells <126980526+inigocastells@users.noreply.github.com>
2023-05-06 11:03:08 +0200

    Update Principal.cpp

--- src/Principal.cpp
+++ src/Principal.cpp
@@ -3,12 +3,17 @@
 #include <ColorCasilla.h>
 #include <MenuInicial.h>
 
+
+#include <ETSIDI.h>
+using ETSIDI::Sprite;
+
 Tablero tablero;
 ColorCasilla color1,color2;
 MenuInicial menu;
-//los callback, funciones que seran llamadas automaticamente por la glut
-//cuando sucedan eventos
+
+//los callback, funciones que seran llamadas automaticamente por la glut cuando sucedan eventos
 //NO HACE FALTA LLAMARLAS EXPLICITAMENTE
+
 void OnDraw(void); //esta funcion sera llamada para dibujar
 void OnTimer(int value); //esta funcion sera llamada cuando transcurra una temporizacion
 void OnKeyboardDown(unsigned char key, int x, int y); //cuando se pulse una tecla	
@@ -54,7 +59,7 @@ void OnDraw(void)
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	gluLookAt(5.5, 4.0, 12.0,  // posicion del ojo (se desplaza un poco en ambos ejes para que cuadre con el tamaño de la pantalla)
+	gluLookAt(5.5, 4.0, 12.0,  // posicion del ojo (se desplaza un poco en ambos ejes para que cuadre con el tamaÃ±o de la pantalla)
 		5.5, 4.0, 0.0,      // hacia que punto mira  (0,0,0) 
 		0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)    
 
@@ -62,32 +67,34 @@ void OnDraw(void)
 	
 
 	//INTRODUCIR AQUI LAS CONDICIONES DEL MENU
-	// PRUEBA DE FUNCIONAMIENTO 
 	menu.dibuja();
-	if(menu.GetOpcion()==10){
-		color1.SetColor(255, 255, 255);
-		color2.SetColor(0, 0, 255);
-	}
-	else if (menu.GetOpcion() == 11) {
-		color1.SetColor(255, 255, 255);
-		color2.SetColor(255, 0, 0);
-	}
-	else if (menu.GetOpcion() == 12) {
-		color1.SetColor(255, 255, 0);
-		color2.SetColor(255, 0, 0);
-	}
 
-	menu.~MenuInicial();
-	glDisable(GL_LIGHTING);
-	tablero.DibujaTablero(color1,color2);
-	glEnable(GL_LIGHTING);
+	if (menu.GetComienzo() == true) {
+		if(menu.GetEquipo()==0){
+			color1.SetColor(255, 255, 255);
+			color2.SetColor(0, 0, 255);
+		}
+		else if (menu.GetEquipo() == 1) {
+			color1.SetColor(255, 255, 255);
+			color2.SetColor(255, 0, 0);
+		}
+		else if (menu.GetEquipo() == 2) {
+			color1.SetColor(250, 189, 0); // COLOR GUALDA
+			color2.SetColor(255, 0, 0);
+		}
+
+		glDisable(GL_LIGHTING);
+		tablero.DibujaTablero(color1,color2);
+		tablero.DibujaPiezas();
+		glEnable(GL_LIGHTING);
+	}
 
 	//no borrar esta linea ni poner nada despues
 	glutSwapBuffers();
 }
 void OnKeyboardDown(unsigned char key, int x_t, int y_t)
 {
-	//poner aqui el código de teclado
+	//poner aqui el cÃ³digo de teclado
 
 	menu.flecha(key);
 
@@ -97,6 +104,8 @@ void OnKeyboardDown(unsigned char key, int x_t, int y_t)
 void flechas(int key, int x_x, int y_y) {
 
 	menu.flecha(key);
+	tablero.MueveCursor(key);
+	
 
 	glutPostRedisplay();
 
@@ -104,7 +113,7 @@ void flechas(int key, int x_x, int y_y) {
 
 void OnTimer(int value)
 {
-	//poner aqui el código de animacion
+	//poner aqui el cÃ³digo de animacion
 
 	//no borrar estas lineas
 	glutTimerFunc(25, OnTimer, 0);
