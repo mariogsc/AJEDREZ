3c4dadb14ec4337ed201bac59fbb37a76097c025
Álvaro Gómez Agudo <80710074+alvarogomez29@users.noreply.github.com>
2023-05-07 11:02:43 +0200

    Create Torre.cpp

---
+++ src/Torre.cpp
@@ -0,0 +1,20 @@
+#include <Torre.h>
+#include <ETSIDI.h>
+#include <freeglut.h>
+
+Torre::Torre(Vector p, COLOR c) {
+	tipo = TORRE;
+	color = c;
+	posicion = p;
+	ImagenTorreN.setCenter(0.0, 0.0);
+	ImagenPeonB.setCenter(0.0, 0.0);
+}
+
+void Torre::Dibuja() { // Dibujamos 
+	glPushMatrix();
+	glTranslatef(posicion.x, posicion.y, 0.1); // Transladamos un poco por arriba del tablero (eje z)
+	if (color == BLANCO) ImagenTorreB.draw();
+	else if (color == NEGRO) ImagenTorreN.draw();
+	glPopMatrix();
+}
+
