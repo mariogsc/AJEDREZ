a56e51a8c64b16817ac470ed7398333a94441a79
Álvaro Gómez Agudo <80710074+alvarogomez29@users.noreply.github.com>
2023-05-07 10:04:43 +0200

    Update Marcador.cpp

--- src/Marcador.cpp
+++ src/Marcador.cpp
@@ -2,12 +2,12 @@
 #include <ETSIDI.h>
 #include "freeglut.h"
 
+Marcador marcador1;
+
 void Marcador::CreaMarcador(ColorCasilla color1,ColorCasilla color2) {
 
-	CartelBlancas->draw();
-	CartelNegras->draw();
-
-	glColor3ub(0, 0, 0);
+	//rectangulo grande derecha
+	marcador1.SetColor(color1);
 	glDisable(GL_LIGHTING);
 	glBegin(GL_POLYGON);
 	glVertex3f(9.8f, 7.3, 0.0f);
@@ -17,7 +17,8 @@ void Marcador::CreaMarcador(ColorCasilla color1,ColorCasilla color2) {
 	glEnd();
 	glEnable(GL_LIGHTING);
 	
-	glColor3ub(0, 0, 0);
+	//rectangulo grande izquierda
+	marcador1.SetColor(color2);
 	glDisable(GL_LIGHTING);
 	glBegin(GL_POLYGON);
 	glVertex3f(8.3f, 7.3, 0.0f);
@@ -26,7 +27,11 @@ void Marcador::CreaMarcador(ColorCasilla color1,ColorCasilla color2) {
 	glVertex3f(8.3f, 7.9, 0.0f);
 	glEnd();
 	glEnable(GL_LIGHTING);
-
+	
+	CartelBlancas->draw();
+	CartelNegras->draw();
+	
+	//rectangulo pequeño derecha
 	glColor3ub(0, 0, 0);
 	glDisable(GL_LIGHTING);
 	glBegin(GL_POLYGON);
@@ -36,7 +41,8 @@ void Marcador::CreaMarcador(ColorCasilla color1,ColorCasilla color2) {
 	glVertex3f(9.8f, 7.2, 0.0f);
 	glEnd();
 	glEnable(GL_LIGHTING);
-
+	
+	//rectangulo pequeño izquierda
 	glColor3ub(0, 0, 0);
 	glDisable(GL_LIGHTING);
 	glBegin(GL_POLYGON);
@@ -47,6 +53,7 @@ void Marcador::CreaMarcador(ColorCasilla color1,ColorCasilla color2) {
 	glEnd();
 	glEnable(GL_LIGHTING);
 	
+	//rectangulo grande blanco
 	glColor3ub(255, 255, 255);
 	glDisable(GL_LIGHTING);
 	glBegin(GL_POLYGON);
