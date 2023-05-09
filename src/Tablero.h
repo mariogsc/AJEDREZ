af4cb5d347e2daca73e63e2d1d68eff91991f482
pedro-alvarez-monteagudo <80712584+pedro-alvarez-monteagudo@users.noreply.github.com>
2023-05-09 10:47:30 +0200

    Update Tablero.h

--- src/Tablero.h
+++ src/Tablero.h
@@ -5,21 +5,29 @@ using ETSIDI::Sprite;
 #include <Piezas.h> 
 #include <ColorCasilla.h>
 #include <Peon.h>
-#include <Torre.h>
-#include <Vector.h>
 #include <NoPieza.h>
+#include <Vector.h>
+//cada pieza
+#include <Peon.h>
+#include <Alfil.h>
+
+
 
 class Tablero
 {
+
 	const int NCasillas = 8; // Creamos una constante del numero de casillas 
 	Casilla** casilla; // Puntero doble de tipo casilla para recorrer las casillas
 	Casilla c;
-	Piezas* lista[64]; // Puntero de tipo Piezas para poder imprimirlas
 	int NPiezas;
+	Piezas* lista[64]; // Puntero de tipo Piezas para poder imprimirlas
+	int turno;
+	int n;
+	
 public: 
 	Tablero(); // Constructor por defecto de tablero
 	void DibujaTablero(ColorCasilla color1,ColorCasilla color2);
-	void DibujaPiezas(); 
+	void DibujaPiezas();
 	void inicializa();
 	bool agregar(Piezas* p);
 	void MueveCursor(unsigned char key);
