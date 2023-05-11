f74e240d6a79a78e42002d067f7ddb4f412aae78
Álvaro Gómez Agudo <80710074+alvarogomez29@users.noreply.github.com>
2023-05-11 09:11:12 +0200

    Update Marcador.cpp

--- src/Marcador.cpp
+++ src/Marcador.cpp
@@ -4,64 +4,6 @@
 
 Marcador marcador1;
 
-void Marcador::CreaMarcador(ColorCasilla color1,ColorCasilla color2) {
+//continuar con marcador.h estructura simplificada
 
-	//rectangulo grande derecha
-	marcador1.SetColor(color1);
-	glDisable(GL_LIGHTING);
-	glBegin(GL_POLYGON);
-	glVertex3f(9.8f, 7.3, 0.0f);
-	glVertex3f(11.1f, 7.3, 0.0f);
-	glVertex3f(11.1f, 7.9, 0.0f);
-	glVertex3f(9.8f, 7.9, 0.0f);
-	glEnd();
-	glEnable(GL_LIGHTING);
-	
-	//rectangulo grande izquierda
-	marcador1.SetColor(color2);
-	glDisable(GL_LIGHTING);
-	glBegin(GL_POLYGON);
-	glVertex3f(8.3f, 7.3, 0.0f);
-	glVertex3f(9.7f, 7.3, 0.0f);
-	glVertex3f(9.7f, 7.9, 0.0f);
-	glVertex3f(8.3f, 7.9, 0.0f);
-	glEnd();
-	glEnable(GL_LIGHTING);
-	
-	CartelBlancas->draw();
-	CartelNegras->draw();
-	
-	//rectangulo pequeño derecha
-	glColor3ub(0, 0, 0);
-	glDisable(GL_LIGHTING);
-	glBegin(GL_POLYGON);
-	glVertex3f(9.8f, 0.1, 0.0f);
-	glVertex3f(11.1f, 0.1, 0.0f);
-	glVertex3f(11.1f, 7.2, 0.0f);
-	glVertex3f(9.8f, 7.2, 0.0f);
-	glEnd();
-	glEnable(GL_LIGHTING);
-	
-	//rectangulo pequeño izquierda
-	glColor3ub(0, 0, 0);
-	glDisable(GL_LIGHTING);
-	glBegin(GL_POLYGON);
-	glVertex3f(8.3f, 0.1, 0.0f);
-	glVertex3f(9.7f, 0.1, 0.0f);
-	glVertex3f(9.7f, 7.2, 0.0f);
-	glVertex3f(8.3f, 7.2, 0.0f);
-	glEnd();
-	glEnable(GL_LIGHTING);
-	
-	//rectangulo grande blanco
-	glColor3ub(255, 255, 255);
-	glDisable(GL_LIGHTING);
-	glBegin(GL_POLYGON);
-	glVertex3f(8.2f, 0.0, 0.0f);
-	glVertex3f(11.2f, 0.0, 0.0f);
-	glVertex3f(11.2f, 8, 0.0f);
-	glVertex3f(8.2f, 8, 0.0f);
-	glEnd();
-	glEnable(GL_LIGHTING);
-}
 
