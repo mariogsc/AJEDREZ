74dac415a2ee6186419e127f87fd74630e0b1def
pedro-alvarez-monteagudo <80712584+pedro-alvarez-monteagudo@users.noreply.github.com>
2023-05-12 13:46:36 +0200

    Update Torre.h

--- src/Torre.h
+++ src/Torre.h
@@ -1,17 +1,21 @@
 #pragma once
+
 #include <ETSIDI.h>
 using ETSIDI::Sprite;
 #include <Piezas.h>
 #include <Vector.h>
 #include <Casilla.h>
+#include <Tablero.h>
+// CREAMOS UNA CLASE TORRE POR HERENCIA DE LA CLASE PIEZAS
 
-class Torre: public Piezas{
+
+class Torre :public Piezas { 
 
 public:
-		Torre(Vector p, COLOR c);
-		Sprite ImagenTorreN{ "imagenes/TorreNegro.png", 0, 0, tam, tam };
-		Sprite ImagenTorreB{ "imagenes/TorreBlanco.png", 0, 0, tam, tam };
-		void Dibuja();
-		bool CheckMov(Vector v);
+	Torre(Vector p, COLOR c); // Constructor para inicializar las variables
+	Sprite ImagenTorreN{ "imagenes/TorreNegro.png", 0, 0, tam, tam };
+	Sprite ImagenTorreB{ "imagenes/TorreBlanco.png", 0, 0, tam, tam };
+	void Dibuja();
+	bool CheckMov(Vector v, int check);
 
 };
