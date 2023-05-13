33b6ede3a7fb58d974753cdd50741d11d0a40e16
Álvaro Gómez Agudo <80710074+alvarogomez29@users.noreply.github.com>
2023-05-13 11:12:59 +0200

    Update Marcador.h

--- src/Marcador.h
+++ src/Marcador.h
@@ -4,29 +4,41 @@
 #include <freeglut.h>
 
 class Marcador {
-    
-    //Finalmente construllo el marcador con la funcion casilla
-    
- Casilla r1;
+     Casilla r1;   
+     Sprite* CartelBlancas = new Sprite("imagenes/CartelBlancas.png", 9, 7.6, 1, 0.4);
+     Sprite* CartelNegras = new Sprite("imagenes/CartelNegras.png", 10.45, 7.6, 1, 0.4);
+
 public:
 
     Marcador() {
-        r1.SetAlto(9.0);
+         r1.SetAlto(8.0);
         r1.SetAncho(3.0);
-        r1.SetColor(200,0,200);
+        r1.SetColor(255,255,255);
+
+        r2.SetAlto(7.2);
+        r2.SetAncho(1.4);
+        r2.SetColor(color1);
+
+        r3.SetAlto(7.2);
+        r3.SetAncho(1.4);
+        r3.SetColor(color2);
+
+        r4.SetAlto(0.6);
+        r4.SetAncho(1.3);
+        r4.SetColor(255, 255, 255);
+
+        r5.SetAlto(0.6);
+        r5.SetAncho(1.3);
+        r5.SetColor(255, 255, 255);
     }
 
 
     void DibujaMarcador() {
-        r1.DibujaCasilla({ 9.0,0.0 }, {});
-
-      /*  r2.DibujaRectangulo(); //continuar y situar los 5 cuadrados para generar un marcador
-        r3.DibujaRectangulo();
-        r4.DibujaRectangulo();
-        r5.DibujaRectangulo();
-
-        */
-        
+        r1.DibujaCasilla({ 8.2,0.0 }, {});
+        r2.DibujaCasilla({ 8.3,0.1 }, {});
+        r3.DibujaCasilla({ 9.8,0.1 }, {});
+        r4.DibujaCasilla({ 8.3, 7.3 }, {});
+        r5.DibujaCasilla({ 9.8, 7.3 }, {});   
     }
 };    
  
