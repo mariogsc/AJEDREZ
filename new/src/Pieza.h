8e7771ca66752c0593f27f9c8728c7b835d857f8
Pedro Alvarez Monteagudo <80712584+pedro-alvarez-monteagudo@users.noreply.github.com>
2023-05-17 12:59:21 +0200

    Update Pieza.h
    Creacion de pieza modificado de l codigo anterior

--- new/src/Pieza.h
+++ new/src/Pieza.h
@@ -1,1 +1,26 @@
+#pragma once
+#include <Vector.h>
 
+class Pieza
+{
+public:
+    enum COLOR { Blanco, Negro };
+    enum TIPO { PEON, REY, REINA, ALFIL, TORRE, CABALLO };
+   protected:
+       COLOR color;
+       TIPO t;
+      
+      //TAMAÑO DE LAS FICHAS
+   public:
+       // Constructor por defecto
+       Pieza(COLOR col) : color(col) {}
+      
+
+      // Método para obtener el tipo de la pieza, se utiliza polimorfismo
+       virtual TIPO getTipo() {};
+
+       // Método para validar un movimiento de la pieza, se uiliza polimorfismo
+       virtual bool validarMovimiento(Vector origen,Vector destino); //Hay que pasarle el tablero o hacerlo friend
+    
+
+};
