001d54024de32fbaca3ca0700533353f8439009e
Álvaro Gómez Agudo <80710074+alvarogomez29@users.noreply.github.com>
2023-05-17 11:02:34 +0200

    Create Mundo.h

---
+++ new/src/Mundo.h
@@ -0,0 +1,19 @@
+#include <MenuInicial.h>
+#include <Tablero.h>
+#include <Color.h>
+
+class Mundo
+{
+	MenuInicial menu;
+	Tablero tablero;
+
+	float x_ojo;
+	float y_ojo;
+	float z_ojo;
+public: 
+	
+	void tecla(unsigned char key);
+	void teclaEspecial(unsigned char key);
+	void dibuja();
+	void inicializa();
+};
