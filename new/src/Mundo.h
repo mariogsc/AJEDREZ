fd14bb8c8d24c0c98649a729a0fd45c796bd7ea4
MARIO GÓMEZ SÁNCHEZ-CELEMÍN <mario.gomez.sanchez-celemin@alumnos.upm.es>
2023-05-19 01:37:17 +0200

    Update Mundo.h

--- new/src/Mundo.h
+++ new/src/Mundo.h
@@ -1,6 +1,6 @@
 #include <MenuInicial.h>
 #include <Tablero.h>
-#include <Color.h>
+#include <ColorRGB.h>
 
 class Mundo
 {
@@ -10,11 +10,16 @@ class Mundo
 	float x_ojo;
 	float y_ojo;
 	float z_ojo;
+
+	Vector origen;
+	Vector destino;
+
 public: 
-	
+
 	void tecla(unsigned char key);
 	void teclaEspecial(unsigned char key);
 	void dibuja();
 	void inicializa();
 	void Raton(int button, int state, int x, int y);
+
 };
