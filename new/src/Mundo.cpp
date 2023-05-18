79051ff47ec61118a632684824ccb36f15e97318
MARIO GÓMEZ SÁNCHEZ-CELEMÍN <mario.gomez.sanchez-celemin@alumnos.upm.es>
2023-05-19 01:36:52 +0200

    Update Mundo.cpp
    Se añade gestión de turnos, control con ratón (arrastrando) y sonidos

--- new/src/Mundo.cpp
+++ new/src/Mundo.cpp
@@ -3,6 +3,7 @@
 #include <math.h>
 #include "cstdlib"
 
+
 void Mundo::inicializa() {
 	x_ojo=5.5;
 	y_ojo=4.0;
@@ -21,21 +22,32 @@ void Mundo::dibuja()
 	menu.dibuja();
 
 	if (menu.GetComienzo() == true) {
+		//Color color1(0,0,0), color2(0,0,0);
+
+		ColorRGB color1(0,0,0), color2(0, 0, 0);
+
 		if (menu.GetEquipo() == 0) {
-			Color color1(255, 255, 255);
-			Color color2(0, 0, 255);
+			color1.SetColor(255, 255, 255);
+			color2.SetColor(0, 0, 255);
+			
 		}
 		else if (menu.GetEquipo() == 1) {
-			Color color1(255, 255, 255);
-			Color color2(255, 0, 0);
+			color1.SetColor(255, 255, 255);
+			color2.SetColor(255, 0, 0);
+			
+			
 		}
 		else if (menu.GetEquipo() == 2) {
-			Color color1(250, 189, 0); // COLOR GUALDA
-			Color color2(255, 0, 0);
-
-			tablero.dibuja(color1, color2);
+			color1.SetColor(250, 189, 0); // COLOR GUALDA
+			color2.SetColor(255, 0, 0);
+			
 		}
+		tablero.DibujaPiezas();
+		tablero.DibujaTablero(color1, color2);
+		tablero.getPieza(origen)->DibujaPosibles();
 	}
+	
+	
 }
 
 void Mundo::teclaEspecial(unsigned char key)
@@ -50,5 +62,52 @@ void Mundo::tecla(unsigned char key)
 }
 
 void Mundo::Raton(int button, int state, int x, int y) {
-	tablero.Raton(button,state,x,y);
+	
+
+	// CONVERSIÓN DE PIXELES DE LA VENTANA A CASILLAS DEL TABLERO
+	x = x/(550/8);
+	y = 7-((y - 24)/(550/8));
+
+	int columna = static_cast<int>(x );
+	int fila = static_cast<int>(y );
+
+	if (((fila >= 0) && (fila < 8)) && ((columna >= 0) && (columna < 8))) {
+		if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+
+
+			if (tablero.turno % 2 != 0) { // Si el turno es impar -> turno blancas
+				if (tablero.getPieza(columna, fila) != nullptr && tablero.getPieza(columna, fila)->getColor() == Pieza::Blanco) {
+
+					origen.x = columna;
+					origen.y = fila;
+					tablero.getPieza(origen)->CompruebaPosible(origen,tablero);
+				}
+				else ETSIDI::play("sonidos/error.wav");
+			}
+			else if (tablero.turno % 2 == 0) { // Si el turno es par -> turno negras
+				if (tablero.getPieza(columna, fila) != nullptr && tablero.getPieza(columna, fila)->getColor() == Pieza::Negro) {
+					origen.x = columna;
+					origen.y = fila;
+				}
+				else ETSIDI::play("sonidos/error.wav");
+			}
+		}
+		else if (button == GLUT_LEFT_BUTTON && state==GLUT_UP) {
+			tablero.getPieza(origen)->eliminarContenido();
+			// Verificar que la posición de origen sea válida y haya una pieza en esa posición
+			if (origen >= 0 && origen < MAX_CASILLAS && tablero.getPieza(origen) != nullptr && origen != destino) {
+				// Verificar si el movimiento es válido
+				destino.x = columna;
+				destino.y = fila;
+
+				if (tablero.getPieza(origen)->validarMovimiento(origen, destino, tablero)) {
+					// Realizar el movimiento
+					tablero.mover(origen, destino); 
+					tablero.turno++;
+				}
+				else ETSIDI::play("sonidos/error.wav");
+			}
+			else ETSIDI::play("sonidos/error.wav");
+		}
+	}
 }
