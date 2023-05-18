31601e0c369f20167ed7cd204078bfdf29fe382e
Pedro Alvarez Monteagudo <80712584+pedro-alvarez-monteagudo@users.noreply.github.com>
2023-05-18 10:52:54 +0200

    Update Vector.h

--- new/src/Vector.h
+++ new/src/Vector.h
@@ -1,1 +1,25 @@
 
+#pragma once
+
+class Vector
+{
+	int x;
+	int y;
+
+public:
+
+	Vector(int _x = 0, int _y = 0) :x(_x), y(_y) {}; // Constructor por defecto con inicialización por defecto a 0
+
+	// FUNCIONES DE INTERFAZ
+	int getx() { return x; }
+	int gety() { return y; }
+	void setx(int _x) { x = _x; }
+	void sety(int _y) { y = _y; }
+	void set(Vector v) { setx(v.x); sety(v.y); }
+
+	// Sobrecarga del operador == para comparar dos vectores
+	bool operator==(const Vector& v) {
+		if (x == v.x && y == v.y)return true;
+		else return false;
+	}
+};
