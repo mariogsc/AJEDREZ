37e30d7cfaef2d1e357285733b1587be583bd728
Iñigo Castells <126980526+inigocastells@users.noreply.github.com>
2023-05-19 13:32:06 +0200

    Add files via upload

---
+++ new/src/Rey.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <ETSIDI.h>
+using ETSIDI::Sprite;
+#include <Pieza.h>
+#include <Vector.h>
+#include <Casilla.h>
+#include <Tablero.h>
+#include <iostream>
+#include <cmath>
+
+class Rey :public Pieza
+{
+
+public:
+	Rey(COLOR c);
+
+	bool validarMovimiento(Vector origen, Vector destino, Tablero& tablero);
+	void DibujaPosible(Vector origen, Tablero& t);
+};
