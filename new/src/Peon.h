ac9381d284b7d23e44d1cd7f00f5bac16c183d88
Pedro Alvarez Monteagudo <80712584+pedro-alvarez-monteagudo@users.noreply.github.com>
2023-05-19 13:56:23 +0200

    Update Peon.h

--- new/src/Peon.h
+++ new/src/Peon.h
@@ -1,1 +1,21 @@
 
+#pragma once
+#include <ETSIDI.h>
+using ETSIDI::Sprite;
+#include <Pieza.h>
+#include <Vector.h>
+#include <Casilla.h>
+#include <Tablero.h>
+#include <iostream>
+#include <cmath>
+
+class Peon :public Pieza
+{
+
+public:
+
+	Peon(COLOR c);
+
+	bool validarMovimiento(Vector origen, Vector destino, Tablero& tablero);
+	void DibujaPosible(Vector origen, Tablero& t);
+};
