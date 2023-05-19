d559192354a04e42dab2e76cc997409757081653
Álvaro Gómez Agudo <80710074+alvarogomez29@users.noreply.github.com>
2023-05-19 09:02:33 +0200

    Update Vector.h

--- new/src/Vector.h
+++ new/src/Vector.h
@@ -10,16 +10,23 @@ public:
 
 	Vector(int _x = 0, int _y = 0) :x(_x), y(_y) {}; // Constructor por defecto con inicialización por defecto a 0
 
-	// FUNCIONES DE INTERFAZ
-	int getx() { return x; }
-	int gety() { return y; }
-	void setx(int _x) { x = _x; }
-	void sety(int _y) { y = _y; }
-	void set(Vector v) { setx(v.x); sety(v.y); }
-
 	// Sobrecarga del operador == para comparar dos vectores
 	bool operator==(const Vector& v) {
 		if (x == v.x && y == v.y)return true;
 		else return false;
+	}
+
+	bool operator != (const Vector& v) {
+		if (x != v.x || y != v.y)return true;
+		else return false;
+	}
+
+	bool operator < (const int& MAX) {
+		if (x < MAX && y < MAX)return true;
+		else return false;
+	}
+	bool operator >= (const int& MAX) {
+		if (x >= MAX && y >= MAX)return true;
+		else return false;
 	}
 };
