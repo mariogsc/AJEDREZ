c0b20b2b390117940587980368ed86d82ebe1356
Álvaro Gómez Agudo <80710074+alvarogomez29@users.noreply.github.com>
2023-05-19 08:55:33 +0200

    Create Alfil.h

---
+++ new/src/Alfil.h
@@ -0,0 +1,13 @@
+using ETSIDI::Sprite;
+#include <Pieza.h>
+#include <Vector.h>
+#include <Casilla.h>
+#include <Tablero.h>
+
+class Alfil:public Pieza
+{	
+public:
+	Alfil(COLOR c);
+
+	bool validarMovimiento(Vector origen, Vector destino, Tablero& t);
+};
