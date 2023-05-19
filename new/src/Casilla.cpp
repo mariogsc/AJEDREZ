3cf7b988e485dd99bb11210d184e461876cf6db2
Iñigo Castells <126980526+inigocastells@users.noreply.github.com>
2023-05-19 12:53:54 +0200

    Update Casilla.cpp

--- new/src/Casilla.cpp
+++ new/src/Casilla.cpp
@@ -13,6 +13,17 @@ void Casilla::DibujaCasilla(Vector posicion, float x = 0.0) {
 	glEnable(GL_LIGHTING);
 }
 
+void Casilla::DibujaCasilla(float x = 0.0) {
+	glDisable(GL_LIGHTING);
+	glBegin(GL_POLYGON);
+	glColor3f(0,255,0);
+	glVertex3f(pos.x+0.1, pos.y+0.1, x);
+	glVertex3f(pos.x + tam-0.1, pos.y+0.1, x);
+	glVertex3f(pos.x + tam-0.1, pos.y + tam-0.1, x);
+	glVertex3f(pos.x+0.1, pos.y + tam-0.1, x);
+	glEnd();
+	glEnable(GL_LIGHTING);
+}
 
 void Casilla::SetColor(unsigned char r ,unsigned char v, unsigned char a) {
 	this->color.SetColor(r, v, a);
