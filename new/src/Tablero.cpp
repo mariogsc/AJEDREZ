61c1281e3cf8dcbe19348ab96fa922b11772704b
Iker Castiella <80773803+Ikercasti@users.noreply.github.com>
2023-05-19 13:12:36 +0200

    Update Tablero.cpp

--- new/src/Tablero.cpp
+++ new/src/Tablero.cpp
@@ -6,17 +6,10 @@
 
 
 
-Tablero::Tablero() :turno(1),numero(0) {
-	// Primero reservamos espacio dinamico para una matriz de 8x8 casillas (tablero) 
-	
-	casilla = new Casilla * [MAX_CASILLAS ];
-
-	for (int i = 0; i < MAX_CASILLAS ; i++) {
-		casilla[i] = new Casilla[MAX_CASILLAS];
-	}
-
-	// Inicializamos el vector de piezas 
+Tablero::Tablero() :NPiezas(0),NLista(0), turno(1) {
 	inicializa();
+	for (int i = 0; i < MAX; i++)lista[i] = nullptr;
+
 }
 
 Tablero::~Tablero() {
@@ -36,14 +29,21 @@ void Tablero::inicializa() {  // inicializacion de todas las fichas e inclusión
 
 	
 	// BLANCAS
-	tablero[5][0] = new Torre(Pieza::Blanco); // COMO PUEDO HACER PARA AL DEFINIR ESTO EL SPRITE SE ME PONGA EN EL 5,0
+	tablero[0][0] = new Torre(Pieza::Blanco);
+	tablero[2][0] = new Alfil(Pieza::Blanco);
 	tablero[7][0] = new Torre(Pieza::Blanco);
-	
-	/*
+	tablero[5][0] = new Alfil(Pieza::Blanco);
+	tablero[1][0] = new Caballo(Pieza::Blanco);
+	tablero[6][0] = new Caballo(Pieza::Blanco);
+
 	// NEGRAS
-	tablero[7][0] = new Torre(Color::Negro);
-	tablero[7][1] = new Caballo(Color::Negro);
-	*/
+	tablero[7][7] = new Torre(Pieza::Negro);
+	tablero[2][7] = new Alfil(Pieza::Negro);
+	tablero[0][7] = new Torre(Pieza::Negro);
+	tablero[5][7] = new Alfil(Pieza::Negro);
+	tablero[1][7] = new Caballo(Pieza::Negro);
+	tablero[6][7] = new Caballo(Pieza::Negro);
+	
 
 	//numero = MAX_PIEZAS;
 }
@@ -55,8 +55,8 @@ void Tablero::DibujaTablero(ColorRGB color1, ColorRGB color2) {
 	float tam = 1.0; //Definimos variables auxiliares para ir desplazando la casilla
 	Vector aux; // incializado por defecto a 0,0
 	for (int columna = 0; columna < MAX_CASILLAS; columna++) {
-		aux.setx(0.0);
-		aux.sety(tam * columna);
+		aux.x=(0.0);
+		aux.y=(tam * columna);
 
 		for (int fila = 0; fila < MAX_CASILLAS; fila++) {
 
@@ -75,7 +75,7 @@ void Tablero::DibujaTablero(ColorRGB color1, ColorRGB color2) {
 			casilla[columna][fila].DibujaCasilla(casilla[columna][fila].GetPos(), {});
 
 
-			aux.setx(aux.getx() + tam);
+			aux.x=(aux.x + tam);
 		}
 	}
 	
@@ -99,9 +99,9 @@ bool Tablero::agregar(Pieza* p,int _i,int _j)
 	if (_i < 0 || _i >= MAX_CASILLAS)return false;
 	if (_j < 0 || _j >= MAX_CASILLAS)return false;
 
-	if (numero < MAX_PIEZAS){
+	if (NPiezas < MAX_PIEZAS){
 		tablero[_i][_j] = p;
-		numero++;
+		NPiezas++;
 		return true;
 	}
 	
@@ -113,7 +113,7 @@ void Tablero::eliminar(int i,int j) {
 
 	delete tablero[i][j];
 	tablero[i][j] = nullptr; // IMPORTANTE PONER EL PUNTERO A nullptr DESPUES DE ELIMINARLO
-	numero--;
+	NPiezas--;
 }
 
 void Tablero::eliminar(Pieza* p) {
@@ -130,78 +130,63 @@ void Tablero::eliminar(Pieza* p) {
 
 // FUNCIONES PARA EL CONTROL DEL MOVIMIENTO Y DIBUJADO
 
-void Tablero::mover(Vector& origen, Vector& destino, Pieza* t[8][8]) {
-	if (origen.getx() < 0 || origen.gety() < 0 || origen.gety() >= 8 || origen.getx() >= 8) return;
-	if (destino.getx() < 0 || destino.gety() < 0 || destino.gety() >= 8 || destino.getx() >= 8) return;
-
-	if (t[destino.getx()][destino.gety()] != nullptr && t[origen.getx()][origen.gety()]->getColor() != t[destino.getx()][destino.gety()]->getColor()) {
-		eliminar(t[destino.getx()][destino.gety()]);
-		t[origen.getx()][origen.gety()] = nullptr;
-	}
-
-	Pieza* temp = t[destino.getx()][destino.gety()];
-	t[destino.getx()][destino.gety()] = t[origen.getx()][origen.gety()];
-	t[origen.getx()][origen.gety()] = temp;
-
-	
-
-}
-
 void Tablero::DibujaPiezas()
 {
 	for (int i = 0; i < MAX_CASILLAS; i++)
 		for (int j = 0; j < MAX_CASILLAS; j++) {
 			if (tablero[i][j] != nullptr) {
+				glPushMatrix();
+				glTranslatef(i, j, 0.01);
 				tablero[i][j]->Dibuja();
+				glPopMatrix();
 			}
 		}
+
 }
 
+void Tablero::mover(Vector origen, Vector destino) {
+	if (origen.x < 0 || origen.y < 0 || origen.y >= 8 || origen.x >= 8) return;
+	if (destino.x < 0 || destino.y < 0 || destino.y >= 8 || destino.x >= 8) return;
 
-void Tablero::Raton(int button, int state, int x, int y) {
-	int fila, columna;
-	Vector origen;
-	Vector destino;
+	if (tablero[destino.x][destino.y] != nullptr && tablero[origen.x][origen.y]->getColor() != tablero[destino.x][destino.y]->getColor()) {
+		eliminar(destino.x,destino.y);
+		ETSIDI::play("sonidos/PiezaComida.wav");
+	}
 
+	tablero[destino.x][destino.y] = tablero[origen.x][origen.y];
+	tablero[origen.x][origen.y] = nullptr; // IMPORTANTE PONER EL PUNTERO A nullptr DESPUES DE MOVER
+}
 
-	if (((x > 22) && (x < 572)) && ((y > 24) && (y < 574))) {
-		if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-
-			x = x - 22;
-			y = y - 24;
-
-			int fila = static_cast<int>(x / 68.75);
-			int columna = static_cast<int>(y / 68.75);
-
-				if (turno % 2 != 0) { // Si el turno es impar -> turno blancas
-					if (tablero[columna][fila] != nullptr && tablero[columna][fila]->getColor() == Pieza::Blanco) {
-
-						origen.setx(columna);
-						origen.sety(fila);
-					}
-				}
-				else if (turno % 2 == 0) { // Si el turno es par -> turno negras
-					if (tablero[columna][fila] != nullptr && tablero[columna][fila]->getColor() == Pieza::Negro) {
-						origen.setx(columna);
-						origen.sety(fila);
-					}
-				}
-		}
-		else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
-
-			// Verificar que la posición de origen sea válida y haya una pieza en esa posición
-			if (origen.getx() >= 0 && origen.getx() < MAX_CASILLAS && origen.gety() >= 0 && origen.gety() < MAX_CASILLAS &&
-				tablero[origen.getx()][origen.gety()] != nullptr) {
-				// Verificar si el movimiento es válido
-				destino.setx(columna);
-				destino.sety(fila);
-
-				if (tablero[origen.getx()][origen.gety()]->validarMovimiento(origen, destino)) {
-					// Realizar el movimiento
-					mover(origen, destino, tablero); // COMO PONE X,Y EN VEZ DE DESTINO
-					turno++;
-				}
+void Tablero::CompruebaPosible(Vector origen) {
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if (this->tablero[origen.x][origen.y]->validarMovimiento(origen, { i,j }, *this) == true)
+			{
+				agregar(new Casilla({ i,j }, { 0, 255, 0 }));
 			}
 		}
+	}
+}
+
+bool Tablero::agregar(Casilla* casilla) {
+	if (NLista < MAX) {
+		lista[NLista++] = casilla;
+		return true;
+	}
+	else return false;
+}
+
+void Tablero::eliminarContenido() {
+	for (int i = 0; i < NLista; i++) {
+		delete lista[i];
+		lista[i] = nullptr;
+	}
+	NLista=0;
+}
+
+void Tablero::DibujaPosibles() {
+	for (int i = 0; i < NLista; i++)
+	{
+		lista[i]->DibujaCasilla(0.0005);
 	}
 }
