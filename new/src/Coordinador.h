075a27be2d49fabfdae1e1e21060295668b71028
Pedro Alvarez Monteagudo <80712584+pedro-alvarez-monteagudo@users.noreply.github.com>
2023-05-25 19:20:45 +0200

    Update Coordinador.h

--- new/src/Coordinador.h
+++ new/src/Coordinador.h
@@ -1,25 +1,33 @@
 #pragma once
 #include <Ajedrez.h>
-
-//TENEMOS QUE METER TODO ESTO EN PRINCIPAL EN VEZ DE AJEDREZ
-
+#include "ETSIDI.h"
+#include <MenuInicial.h>
+using ETSIDI::Sprite;
 
 class Coordinador
 {
+	int sobrecargaintro = 0;
+
+	float x_ojo;
+	float y_ojo;
+	float z_ojo;
+
 
 public:
-	Coordinador() { estado = INICIO; };
+	Coordinador();
 	virtual ~Coordinador() {};
 
 	//void mueve();
 	//nose si haria falta mueve ya que lo metemos en raton el movimiento revisar
+	void inicializa();
 	void dibuja();
 	void tecla(unsigned char key);
+	void teclaEspecial(unsigned char key);
 	void Raton(int button, int state, int x, int y);
-	
+
 protected:
-	
-	enum Estado {INICIO,MENU, SELECCION, PARTIDA,PAUSA, GANA_B, GANA_N }; 
+
+	enum Estado { INICIO, MENU, PARTIDA, PAUSA, GANA_B, GANA_N, TABLAS };
 
 	Estado estado;
 	Ajedrez ajedrez;
